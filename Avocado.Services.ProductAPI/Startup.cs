386fedf69ba02d7c1f86cbb1468ca46b6a977728
Deni22e <griseldadenisfernandez@gmail.com>
2022-04-11 19:44:30 -0300

    ProductAPI Config

--- Avocado.Services.ProductAPI/Startup.cs
+++ Avocado.Services.ProductAPI/Startup.cs
@@ -1,17 +1,19 @@
-using Avocado.Web.Services;
-using Avocado.Web.Services.IServices;
+using Avocado.Services.ProductAPI.DbContexts;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Avocado.Web
+namespace Avocado.Services.ProductAPI
 {
 	public class Startup
 	{
@@ -25,9 +27,11 @@ namespace Avocado.Web
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
-			services.AddScoped<IBaseService, BaseService>();
-			services.AddHttpClient<IProductService, ProductService>();
+			services.AddControllers();
+			services.AddDbContext<ApplicationDbContext>(x =>
+			{
+				x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,14 +41,8 @@ namespace Avocado.Web
 			{
 				app.UseDeveloperExceptionPage();
 			}
-			else
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+
 			app.UseHttpsRedirection();
-			app.UseStaticFiles();
 
 			app.UseRouting();
 
@@ -52,9 +50,7 @@ namespace Avocado.Web
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllerRoute(
-					name: "default",
-					pattern: "{controller=Home}/{action=Index}/{id?}");
+				endpoints.MapControllers();
 			});
 		}
 	}

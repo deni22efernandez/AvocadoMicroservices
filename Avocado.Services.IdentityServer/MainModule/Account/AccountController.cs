597bf15b78c539d4b4707b1038de88059ff674ec
Deni22e <griseldadenisfernandez@gmail.com>
2022-04-20 20:08:35 -0300

    IdentityServer user registration

--- Avocado.Services.IdentityServer/MainModule/Account/AccountController.cs
+++ Avocado.Services.IdentityServer/MainModule/Account/AccountController.cs
@@ -19,6 +19,9 @@ using Duende.IdentityServer.Stores;
 using Duende.IdentityServer.Test;
 using Avocado.Services.IdentityServer.Models;
 using Microsoft.AspNetCore.Identity;
+using Avocado.Services.IdentityServer.MainModule.Account;
+using System.Security.Claims;
+using System.Collections.Generic;
 
 namespace IdentityServerHost.Quickstart.UI
 {
@@ -251,20 +254,97 @@ namespace IdentityServerHost.Quickstart.UI
 		{
 			return View();
 		}
+		[HttpGet]
+		public IActionResult Register()
+		{
+			return View(new RegisterViewModel());
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Register(RegisterViewModel vm)
+		{
+			if (ModelState.IsValid)
+			{
+				ApplicationUser newUser = new ApplicationUser
+				{
+					Email = vm.Email,
+					EmailConfirmed = true,
+					LastName = vm.LastName,
+					Name = vm.FirstName,
+					UserName = vm.Username
+				};
+				var result = await _userManager.CreateAsync(newUser, vm.Password);
+				if (result.Succeeded)
+				{
 
+					if (!await _roleManager.RoleExistsAsync(vm.RoleName))
+					{
+						await _roleManager.CreateAsync(new IdentityRole { Name = vm.RoleName, NormalizedName = vm.RoleName });
+					}
+					await _userManager.AddToRoleAsync(newUser, vm.RoleName);
 
-		/*****************************************/
-		/* helper APIs for the AccountController */
-		/*****************************************/
-		private async Task<LoginViewModel> BuildLoginViewModelAsync(string returnUrl)
+					await _userManager.AddClaimsAsync(newUser, new Claim[]
+					{
+						new Claim(JwtClaimTypes.Name, vm.Username),
+						new Claim(JwtClaimTypes.Email, vm.Email),
+						new Claim(JwtClaimTypes.Role, vm.RoleName),
+						new Claim(JwtClaimTypes.GivenName, vm.FirstName),
+						new Claim(JwtClaimTypes.FamilyName, vm.LastName)
+					});
+					var context = await _interaction.GetAuthorizationContextAsync(vm.ReturnUrl);
+					var loginResult = await _signInManager.PasswordSignInAsync(newUser, vm.Password, false, false);
+					if (loginResult.Succeeded)
+					{
+						var checkuser = await _userManager.FindByNameAsync(vm.Username);
+						await _events.RaiseAsync(new UserLoginSuccessEvent(checkuser.UserName, checkuser.Id, checkuser.UserName, clientId: context?.Client.ClientId));
+
+						if (context != null)
+						{
+							if (context.IsNativeClient())
+							{
+								// The client is native, so this change in how to
+								// return the response is for better UX for the end user.
+								return this.LoadingPage("Redirect", vm.ReturnUrl);
+							}
+
+							// we can trust model.ReturnUrl since GetAuthorizationContextAsync returned non-null
+							return Redirect(vm.ReturnUrl);
+						}
+
+						// request for a local page
+						if (Url.IsLocalUrl(vm.ReturnUrl))
+						{
+							return Redirect(vm.ReturnUrl);
+						}
+						else if (string.IsNullOrEmpty(vm.ReturnUrl))
+						{
+							return Redirect("~/");
+						}
+						else
+						{
+							// user might have clicked on a malicious link - should be logged
+							throw new Exception("invalid return URL");
+						}
+					}
+
+				}
+				
+			}
+			return View(vm);
+		}
+		private async Task<RegisterViewModel> BuildRegisterViewModelAsync(string returnUrl)
 		{
 			var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
+			List<string> roles = new List<string>();
+			roles.Add("Admin");
+			roles.Add("Customer");
+			ViewBag.message = roles;
 			if (context?.IdP != null && await _schemeProvider.GetSchemeAsync(context.IdP) != null)
 			{
 				var local = context.IdP == Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider;
 
 				// this is meant to short circuit the UI and only trigger the one external IdP
-				var vm = new LoginViewModel
+				var vm = new RegisterViewModel
 				{
 					EnableLocalLogin = local,
 					ReturnUrl = returnUrl,
@@ -304,7 +384,7 @@ namespace IdentityServerHost.Quickstart.UI
 				}
 			}
 
-			return new LoginViewModel
+			return new RegisterViewModel
 			{
 				AllowRememberLogin = AccountOptions.AllowRememberLogin,
 				EnableLocalLogin = allowLocal && AccountOptions.AllowLocalLogin,
@@ -314,74 +394,135 @@ namespace IdentityServerHost.Quickstart.UI
 			};
 		}
 
-		private async Task<LoginViewModel> BuildLoginViewModelAsync(LoginInputModel model)
-		{
-			var vm = await BuildLoginViewModelAsync(model.ReturnUrl);
-			vm.Username = model.Username;
-			vm.RememberLogin = model.RememberLogin;
-			return vm;
-		}
-
-		private async Task<LogoutViewModel> BuildLogoutViewModelAsync(string logoutId)
-		{
-			var vm = new LogoutViewModel { LogoutId = logoutId, ShowLogoutPrompt = AccountOptions.ShowLogoutPrompt };
-
-			if (User?.Identity.IsAuthenticated != true)
+		/*****************************************/
+		/* helper APIs for the AccountController */
+		/*****************************************/
+		private async Task<LoginViewModel> BuildLoginViewModelAsync(string returnUrl)
 			{
-				// if the user is not authenticated, then just show logged out page
-				vm.ShowLogoutPrompt = false;
-				return vm;
-			}
-
-			var context = await _interaction.GetLogoutContextAsync(logoutId);
-			if (context?.ShowSignoutPrompt == false)
-			{
-				// it's safe to automatically sign-out
-				vm.ShowLogoutPrompt = false;
-				return vm;
-			}
-
-			// show the logout prompt. this prevents attacks where the user
-			// is automatically signed out by another malicious web page.
-			return vm;
-		}
-
-		private async Task<LoggedOutViewModel> BuildLoggedOutViewModelAsync(string logoutId)
-		{
-			// get context information (client name, post logout redirect URI and iframe for federated signout)
-			var logout = await _interaction.GetLogoutContextAsync(logoutId);
-
-			var vm = new LoggedOutViewModel
-			{
-				AutomaticRedirectAfterSignOut = AccountOptions.AutomaticRedirectAfterSignOut,
-				PostLogoutRedirectUri = logout?.PostLogoutRedirectUri,
-				ClientName = string.IsNullOrEmpty(logout?.ClientName) ? logout?.ClientId : logout?.ClientName,
-				SignOutIframeUrl = logout?.SignOutIFrameUrl,
-				LogoutId = logoutId
-			};
-
-			if (User?.Identity.IsAuthenticated == true)
-			{
-				var idp = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
-				if (idp != null && idp != Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider)
+				var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
+				if (context?.IdP != null && await _schemeProvider.GetSchemeAsync(context.IdP) != null)
 				{
-					var providerSupportsSignout = await HttpContext.GetSchemeSupportsSignOutAsync(idp);
-					if (providerSupportsSignout)
-					{
-						if (vm.LogoutId == null)
-						{
-							// if there's no current logout context, we need to create one
-							// this captures necessary info from the current logged in user
-							// before we signout and redirect away to the external IdP for signout
-							vm.LogoutId = await _interaction.CreateLogoutContextAsync();
-						}
+					var local = context.IdP == Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider;
 
-						vm.ExternalAuthenticationScheme = idp;
+					// this is meant to short circuit the UI and only trigger the one external IdP
+					var vm = new LoginViewModel
+					{
+						EnableLocalLogin = local,
+						ReturnUrl = returnUrl,
+						Username = context?.LoginHint,
+					};
+
+					if (!local)
+					{
+						vm.ExternalProviders = new[] { new ExternalProvider { AuthenticationScheme = context.IdP } };
+					}
+
+					return vm;
+				}
+
+				var schemes = await _schemeProvider.GetAllSchemesAsync();
+
+				var providers = schemes
+					.Where(x => x.DisplayName != null)
+					.Select(x => new ExternalProvider
+					{
+						DisplayName = x.DisplayName ?? x.Name,
+						AuthenticationScheme = x.Name
+					}).ToList();
+
+				var allowLocal = true;
+				if (context?.Client.ClientId != null)
+				{
+					var client = await _clientStore.FindEnabledClientByIdAsync(context.Client.ClientId);
+					if (client != null)
+					{
+						allowLocal = client.EnableLocalLogin;
+
+						if (client.IdentityProviderRestrictions != null && client.IdentityProviderRestrictions.Any())
+						{
+							providers = providers.Where(provider => client.IdentityProviderRestrictions.Contains(provider.AuthenticationScheme)).ToList();
+						}
 					}
 				}
+
+				return new LoginViewModel
+				{
+					AllowRememberLogin = AccountOptions.AllowRememberLogin,
+					EnableLocalLogin = allowLocal && AccountOptions.AllowLocalLogin,
+					ReturnUrl = returnUrl,
+					Username = context?.LoginHint,
+					ExternalProviders = providers.ToArray()
+				};
 			}
 
-			return vm;
+			private async Task<LoginViewModel> BuildLoginViewModelAsync(LoginInputModel model)
+			{
+				var vm = await BuildLoginViewModelAsync(model.ReturnUrl);
+				vm.Username = model.Username;
+				vm.RememberLogin = model.RememberLogin;
+				return vm;
+			}
+
+			private async Task<LogoutViewModel> BuildLogoutViewModelAsync(string logoutId)
+			{
+				var vm = new LogoutViewModel { LogoutId = logoutId, ShowLogoutPrompt = AccountOptions.ShowLogoutPrompt };
+
+				if (User?.Identity.IsAuthenticated != true)
+				{
+					// if the user is not authenticated, then just show logged out page
+					vm.ShowLogoutPrompt = false;
+					return vm;
+				}
+
+				var context = await _interaction.GetLogoutContextAsync(logoutId);
+				if (context?.ShowSignoutPrompt == false)
+				{
+					// it's safe to automatically sign-out
+					vm.ShowLogoutPrompt = false;
+					return vm;
+				}
+
+				// show the logout prompt. this prevents attacks where the user
+				// is automatically signed out by another malicious web page.
+				return vm;
+			}
+
+			private async Task<LoggedOutViewModel> BuildLoggedOutViewModelAsync(string logoutId)
+			{
+				// get context information (client name, post logout redirect URI and iframe for federated signout)
+				var logout = await _interaction.GetLogoutContextAsync(logoutId);
+
+				var vm = new LoggedOutViewModel
+				{
+					AutomaticRedirectAfterSignOut = AccountOptions.AutomaticRedirectAfterSignOut,
+					PostLogoutRedirectUri = logout?.PostLogoutRedirectUri,
+					ClientName = string.IsNullOrEmpty(logout?.ClientName) ? logout?.ClientId : logout?.ClientName,
+					SignOutIframeUrl = logout?.SignOutIFrameUrl,
+					LogoutId = logoutId
+				};
+
+				if (User?.Identity.IsAuthenticated == true)
+				{
+					var idp = User.FindFirst(JwtClaimTypes.IdentityProvider)?.Value;
+					if (idp != null && idp != Duende.IdentityServer.IdentityServerConstants.LocalIdentityProvider)
+					{
+						var providerSupportsSignout = await HttpContext.GetSchemeSupportsSignOutAsync(idp);
+						if (providerSupportsSignout)
+						{
+							if (vm.LogoutId == null)
+							{
+								// if there's no current logout context, we need to create one
+								// this captures necessary info from the current logged in user
+								// before we signout and redirect away to the external IdP for signout
+								vm.LogoutId = await _interaction.CreateLogoutContextAsync();
+							}
+
+							vm.ExternalAuthenticationScheme = idp;
+						}
+					}
+				}
+
+				return vm;
+			}
 		}
 	}
-}
